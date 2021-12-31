a2c44ee3c9787616f7283be9f9652d938158d187
jangidjay <jangid321jay@gmail.com>
2021-12-31 13:24:54 +0530

    more useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -6,7 +6,18 @@ const UseStateBasics = () => {
   // const value = useState(1)[0];
   // const handler = useState(1)[1];
   // console.log(value, handler);
+  const image01 = require("./img/goku.jpg");
+  const image02 = require("./img/luffy.jpeg");
   const [text, setText] = useState("random title");
+  const [image, setImage] = useState(image01);
+
+  const changeImage = () => {
+    if (image === image01) {
+      setImage(image02);
+    } else {
+      setImage(image01);
+    }
+  };
 
   const handleClick = () => {
     if (text === "random title") {
@@ -19,8 +30,14 @@ const UseStateBasics = () => {
   return (
     <React.Fragment>
       <h1>{text}</h1>
+      <h2>
+        <img src={image} alt="" style={{ height: 400, width: 400 }} />
+      </h2>
       <button className="btn" onClick={handleClick}>
         change title
+      </button>
+      <button className="btn" onClick={changeImage}>
+        change image
       </button>
     </React.Fragment>
   );
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  let [value, setValue] = useState(0);
+
+  // const decValue = () => {
+  //   value -= 1;
+  //   setValue(value);
+  // };
+
+  // const resValue = () => {
+  //   setValue(0);
+  // };
+
+  // const incValue = () => {
+  //   value += 1;
+  //   setValue(value);
+  // };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
